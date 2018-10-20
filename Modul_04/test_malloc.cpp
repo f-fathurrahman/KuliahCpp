950f3eab56c4abfac9f20c7fedeb3a6db9c76aaa
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2018-10-20 09:47:26 +0700

    Versi interaktif untuk test_malloc

--- Modul_04/test_malloc.cpp
+++ Modul_04/test_malloc.cpp
@@ -4,10 +4,23 @@ using namespace std;
 
 int main()
 {
-  const int Nsize = 4;
+  int Nsize;
   float *nilai;
 
+  cout << "Masukkan jumlah data: ";
+  cin >> Nsize;
+
   nilai = (float*)malloc(Nsize*sizeof(float));
+
+  for(int i = 0; i < Nsize; i++) {
+    cout << "Masukkan data ke " << i+1 << endl;
+    cin >> nilai[i];
+  }
+
+  cout << "Data yang Anda masukkan adalah" << endl;
+  for(int i = 0; i < Nsize; i++) {
+    cout << nilai[i] << endl;
+  }
 
   free(nilai);
 
